067bf87ba746e3b25b08a5948ea01fd7b85e2a4e
Unknown <son.trinh@employmenthero.com>
2018-09-06 10:43:30 +0700

    modify contract_block schema

--- src/schema/contractBlock.js
+++ src/schema/contractBlock.js
@@ -1,6 +1,6 @@
 export const contractBlockSchema = editor => {
 	editor.model.schema.register('contract_block', {
-		allowIn: ['contract_section', 'contract_block'],
+		allowIn: ['contract_section', 'contract_block', 'paragraph'],
 		allowAttributes: [
 			'block_group',
 			'id',
