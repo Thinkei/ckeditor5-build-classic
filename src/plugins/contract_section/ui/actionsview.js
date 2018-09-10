37a40210f4a1b24e98166a8b0ee503805212a096
Unknown <son.trinh@employmenthero.com>
2018-09-10 11:15:05 +0700

    define section's action view

---
+++ src/plugins/contract_section/ui/actionsview.js
@@ -0,0 +1,40 @@
+import View from '@ckeditor/ckeditor5-ui/src/view';
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+import bulletedlistIcon from '@ckeditor/ckeditor5-list/theme/icons/bulletedlist.svg';
+import numberedlistIcon from '@ckeditor/ckeditor5-list/theme/icons/numberedlist.svg';
+
+export default class SectionActionView extends View {
+	constructor(locale) {
+		super(locale);
+		const t = locale.t;
+
+		this.changeTitleButtonView = this.createButton(
+			t('Edit Title'),
+			bulletedlistIcon,
+			'editTitle'
+		);
+
+		this.setTemplate({
+			tag: 'div',
+			attributes: {
+				class: ['ck', 'ck-link-actions'],
+				tabindex: '-1'
+			},
+			children: [this.changeTitleButtonView]
+		});
+	}
+
+	createButton(label, icon, eventName) {
+		const button = new ButtonView(this.locale);
+
+		button.set({
+			label,
+			icon,
+			tooltip: true
+		});
+
+		button.delegate('execute').to(this, eventName);
+
+		return button;
+	}
+}
