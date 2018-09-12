d2fc111d3b8d9104ea975e0d5484e8e2cf918e3e
Unknown <son.trinh@employmenthero.com>
2018-09-12 14:33:54 +0700

    Add addVarString command to editor

--- src/plugins/variable_string/variablestringediting.js
+++ src/plugins/variable_string/variablestringediting.js
@@ -1,7 +1,10 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import { AddVarStringCommand } from './variablestringcommand';
 
 export default class VariableStringEditing extends Plugin {
 	init() {
-		console.log('editing');
+		const editor = this.editor;
+
+		editor.commands.add('addVarString', new AddVarStringCommand(editor));
 	}
 }
