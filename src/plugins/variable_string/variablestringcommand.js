d604eb3f9a7cf2cc4387f1f2501c78e6be7ccfaf
Unknown <son.trinh@employmenthero.com>
2018-09-12 15:47:56 +0700

    insert Variable to model document

--- src/plugins/variable_string/variablestringcommand.js
+++ src/plugins/variable_string/variablestringcommand.js
@@ -1,11 +1,13 @@
 import Command from '@ckeditor/ckeditor5-core/src/command';
 
+import { addVariable } from './utils';
+
 export class AddVarStringCommand extends Command {
 	refresh() {
 		this.isEnabled = true;
 	}
 
 	execute() {
-		console.log('add var string');
+		addVariable('String', this.editor);
 	}
 }
