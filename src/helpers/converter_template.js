dd2004f1da6d9bd84227d630c72ef1c257872b5e
Unknown <son.trinh@employmenthero.com>
2018-09-13 13:47:52 +0700

    refactor code

--- src/helpers/converter_template.js
+++ src/helpers/converter_template.js
@@ -1,60 +1,13 @@
 import { upcastElementToElement } from '@ckeditor/ckeditor5-engine/src/conversion/upcast-converters';
 import { downcastElementToElement } from '@ckeditor/ckeditor5-engine/src/conversion/downcast-converters';
 
-import { toBool } from '../plugins/contract_block/utils';
-
-const mapModelToHTML = {
-	contract_section: 'section',
-	contract_block: 'section',
-	section_title: 'p',
-	variable_string: 'span',
-	variable_image: 'span',
-	variable_date: 'span',
-	variable_signature_pad: 'span',
-	variable_select: 'span',
-	table: 'table',
-	tbody: 'tbody',
-	tr: 'tr',
-	td: 'td'
-};
-
-const blockElementSymbol = 'blockElement';
-const SECTION_TITLE = 'sectionTitle';
-const SECTION_ELEMENT = 'sectionElement';
-
-const createBlockElement = (viewWriter, modelElement, htmlTagName) => {
-	const blockElement = viewWriter.createContainerElement(
-		mapModelToHTML[htmlTagName],
-		modelElement._attrs
-	);
-
-	viewWriter.setCustomProperty(blockElementSymbol, true, blockElement);
-	if (toBool(blockElement.getAttribute('optional')))
-		viewWriter.addClass('contract-block', blockElement);
-	else viewWriter.addClass('contract-block-dotted', blockElement);
-
-	return blockElement;
-};
-
-const createSectionTitleElement = (viewWriter, modelElement, htmlTagName) => {
-	const sectionTitleElement = viewWriter.createContainerElement(
-		mapModelToHTML[htmlTagName],
-		modelElement._attrs
-	);
-
-	viewWriter.setCustomProperty(SECTION_TITLE, true, sectionTitleElement);
-	return sectionTitleElement;
-};
-
-const createSectionElement = (viewWriter, modelElement, htmlTagName) => {
-	const sectionElement = viewWriter.createContainerElement(
-		mapModelToHTML[htmlTagName],
-		modelElement._attrs
-	);
-
-	viewWriter.setCustomProperty(SECTION_ELEMENT, true, sectionElement);
-	return sectionElement;
-};
+import {
+	createBlockElement,
+	createSectionElement,
+	createSectionTitleElement,
+	createVarStringElement,
+	mapModelToHTML
+} from './utils';
 
 export const converterHelperTemplate = (editor, htmlTagName) => {
 	// upcast helper
@@ -110,6 +63,13 @@ export const converterHelperTemplate = (editor, htmlTagName) => {
 					}
 					case 'contract_section': {
 						return createSectionElement(
+							viewWriter,
+							modelElement,
+							htmlTagName
+						);
+					}
+					case 'variable_string': {
+						return createVarStringElement(
 							viewWriter,
 							modelElement,
 							htmlTagName
--- src/plugins/variable_string/utils.js
+++ src/plugins/variable_string/utils.js
@@ -1,5 +1,6 @@
-import toMap from '@ckeditor/ckeditor5-utils/src/tomap';
 import ModelPosition from '@ckeditor/ckeditor5-engine/src/model/position';
+import Range from '@ckeditor/ckeditor5-engine/src/model/range';
+import { toWidget } from '@ckeditor/ckeditor5-widget/src/utils';
 
 export const variableStringAttributes = {
 	auto_populate: 'organisation_name',
@@ -13,11 +14,25 @@ export const variableStringAttributes = {
 	class: 'variable-string'
 };
 
+export const toVariableWidget = (viewElement, viewWriter) => {
+	return toWidget(viewElement, viewWriter);
+};
+
 const genVariableAttribute = type => {
 	return {
 		...variableStringAttributes,
 		variable_type: type
 	};
+};
+
+const createVarStringModelElement = (modelWriter, position, type) => {
+	const variableTag = modelWriter.createElement(
+		'variable_string',
+		genVariableAttribute(type)
+	);
+	modelWriter.append(modelWriter.createText(`Variable-${type}`), variableTag);
+	modelWriter.insert(variableTag, position);
+	modelWriter.setSelection(Range.createOn(variableTag));
 };
 
 export const addVariable = (type, editor) => {
@@ -26,36 +41,24 @@ export const addVariable = (type, editor) => {
 			editor.model.change(modelWriter => {
 				const selection = editor.model.document.selection;
 				if (selection.isCollapsed) {
-					const variableTag = modelWriter.createElement(
-						'variable_string',
-						genVariableAttribute(type)
-					);
-					modelWriter.append(
-						modelWriter.createText(`Variable-${type}`),
-						variableTag
-					);
-					modelWriter.insert(
-						variableTag,
-						selection.getFirstPosition()
+					createVarStringModelElement(
+						modelWriter,
+						selection.getFirstPosition(),
+						type
 					);
 				} else {
 					const ranges = editor.model.schema.getValidRanges(
 						selection.getRanges(),
 						'variable'
 					);
-
 					for (const range of ranges) {
 						const position = ModelPosition.createAt(range.start);
 						modelWriter.remove(range);
-						const variableTag = modelWriter.createElement(
-							'variable_string',
-							genVariableAttribute(type)
+						createVarStringModelElement(
+							modelWriter,
+							position,
+							type
 						);
-						modelWriter.append(
-							modelWriter.createText(`Variable-${type}`),
-							variableTag
-						);
-						modelWriter.insert(variableTag, position);
 					}
 				}
 			});
