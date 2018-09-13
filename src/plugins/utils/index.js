651758e9b49aaeec78b06f35bc1cb0823c6d2421
Unknown <son.trinh@employmenthero.com>
2018-09-13 17:39:50 +0700

    introduce function to insert variable element to model document

--- src/plugins/utils/index.js
+++ src/plugins/utils/index.js
@@ -1,6 +1,5 @@
 import ModelPosition from '@ckeditor/ckeditor5-engine/src/model/position';
 import Range from '@ckeditor/ckeditor5-engine/src/model/range';
-import { toWidget } from '@ckeditor/ckeditor5-widget/src/utils';
 
 export const variableStringAttributes = {
 	auto_populate: 'organisation_name',
@@ -11,41 +10,42 @@ export const variableStringAttributes = {
 	required: false,
 	variable_name: 'hello',
 	variable_type: 'String',
-	class: 'variable-string'
+	class: ''
 };
 
-export const toVariableWidget = (viewElement, viewWriter) => {
-	return toWidget(viewElement, viewWriter);
-};
-
-const genVariableAttribute = type => {
+const genVariableAttributes = (type, className) => {
 	return {
 		...variableStringAttributes,
-		variable_type: type
+		variable_type: type,
+		class: className
 	};
 };
 
-const createVarStringModelElement = (modelWriter, position, type) => {
+const createVariableElement = (modelWriter, position, type, className) => {
 	const variableTag = modelWriter.createElement(
-		'variable_string',
-		genVariableAttribute(type)
+		`variable_${type}`,
+		genVariableAttributes(type, className)
 	);
-	modelWriter.append(modelWriter.createText(`Variable-${type}`), variableTag);
+	modelWriter.append(modelWriter.createText(`variable-${type}`), variableTag);
 	modelWriter.insert(variableTag, position);
 	modelWriter.setSelection(Range.createOn(variableTag));
 };
 
+// Add variable element to model
 // TODO: define case for each 'type' when add new varible
+// TODO: refactor code here (abtract function for changing model)
+// TODO: re-define css for this class
 export const addVariable = (type, editor) => {
 	switch (type) {
-		case 'String': {
+		case 'string': {
 			editor.model.change(modelWriter => {
 				const selection = editor.model.document.selection;
 				if (selection.isCollapsed) {
-					createVarStringModelElement(
+					createVariableElement(
 						modelWriter,
 						selection.getFirstPosition(),
-						type
+						type,
+						`variable_${type}`
 					);
 				} else {
 					const ranges = editor.model.schema.getValidRanges(
@@ -55,23 +55,26 @@ export const addVariable = (type, editor) => {
 					for (const range of ranges) {
 						const position = ModelPosition.createAt(range.start);
 						modelWriter.remove(range);
-						createVarStringModelElement(
+						createVariableElement(
 							modelWriter,
 							position,
-							type
+							type,
+							`variable_${type}`
 						);
 					}
 				}
 			});
+			return;
 		}
-		case 'Select': {
+		case 'select': {
 			editor.model.change(modelWriter => {
 				const selection = editor.model.document.selection;
 				if (selection.isCollapsed) {
-					createVarStringModelElement(
+					createVariableElement(
 						modelWriter,
 						selection.getFirstPosition(),
-						type
+						type,
+						`variable_${type}`
 					);
 				} else {
 					const ranges = editor.model.schema.getValidRanges(
@@ -81,14 +84,45 @@ export const addVariable = (type, editor) => {
 					for (const range of ranges) {
 						const position = ModelPosition.createAt(range.start);
 						modelWriter.remove(range);
-						createVarStringModelElement(
+						createVariableElement(
 							modelWriter,
 							position,
-							type
+							type,
+							`variable_${type}`
 						);
 					}
 				}
 			});
+			return;
+		}
+		case 'date': {
+			editor.model.change(modelWriter => {
+				const selection = editor.model.document.selection;
+				if (selection.isCollapsed) {
+					createVariableElement(
+						modelWriter,
+						selection.getFirstPosition(),
+						type,
+						`variable_${type}`
+					);
+				} else {
+					const ranges = editor.model.schema.getValidRanges(
+						selection.getRanges(),
+						'variable'
+					);
+					for (const range of ranges) {
+						const position = ModelPosition.createAt(range.start);
+						modelWriter.remove(range);
+						createVariableElement(
+							modelWriter,
+							position,
+							type,
+							`variable_${type}`
+						);
+					}
+				}
+			});
+			return;
 		}
 	}
 };
