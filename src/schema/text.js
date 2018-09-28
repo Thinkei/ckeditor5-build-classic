84340b5db9756e390bd1c899961656d103154196
Son Trinh <son.trinh@employmenthero.com>
2018-09-28 11:34:37 +0700

    update schema & upgrade version

--- src/schema/text.js
+++ src/schema/text.js
@@ -1,16 +1,15 @@
 export const textSchema = editor => {
 	editor.model.schema.extend('$text', {
-		allowAttributes: ['variable'],
 		allowIn: [
 			'paragraph',
-			'td',
-			'contract_section',
-			'contract_block',
 			'variable_string',
 			'variable_date',
 			'variable_image',
 			'variable_signature_pad',
 			'variable_select',
+			'td',
+			'contract_section',
+			'contract_block',
 			'section_title'
 		]
 	});
