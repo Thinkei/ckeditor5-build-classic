b03447beb38c43db07dc6689fdf276f8b0e0be72
Son Trinh <son.trinh@employmenthero.com>
2018-10-03 14:16:48 +0700

    fix press Enter split contract block element

--- src/schema/contractBlock.js
+++ src/schema/contractBlock.js
@@ -1,5 +1,6 @@
 export function contractBlockSchema(editor) {
 	editor.model.schema.register('contract_block', {
+		isLimit: true,
 		allowIn: ['contract_section', 'contract_block', 'paragraph'],
 		allowAttributes: [
 			'block_group',
