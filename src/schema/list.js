08d37793f00f5062767a7a204455a1dc398ac3a2
Son Trinh <son.trinh@employmenthero.com>
2018-10-17 09:50:57 +0700

    tweak list schema

--- src/schema/list.js
+++ src/schema/list.js
@@ -1,6 +1,6 @@
 export const listItemSchema = editor => {
 	editor.model.schema.extend('listItem', {
-		allowIn: ['contract_block'],
+		allowIn: ['contract_block', 'listItem'],
 		allowAttributes: [
 			'style',
 			'class',
